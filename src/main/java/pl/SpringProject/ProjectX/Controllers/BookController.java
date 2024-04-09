34471b6f3c6db9c0caff696d696681bde598f47d
Hexan4 <krystian060203@gmail.com>
2024-04-09 18:16:27 +0200

    Connected H2, play with jpa etc.

--- src/main/java/pl/SpringProject/ProjectX/Controllers/BookController.java
+++ src/main/java/pl/SpringProject/ProjectX/Controllers/BookController.java
@@ -1,21 +1,27 @@
 package pl.SpringProject.ProjectX.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.SpringProject.ProjectX.Data.DataBook;
+import pl.SpringProject.ProjectX.Repository.BookRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
 
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     @GetMapping
     public List<DataBook> getAllBooks() {
-        List<DataBook> books = new ArrayList<>();
-        books.add(new DataBook(1L, "The Hitchhiker's Guide to the Galaxy", "Douglas Adams"));
-        books.add(new DataBook(2L, "1984", "George Orwell"));
-        return books;
+        return bookRepository.findAll();
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO data_book (title, author) VALUES ('1984', 'George Orwell');
+INSERT INTO data_book (title, author) VALUES ('jas i malgosia', 'dziadek mroz');
