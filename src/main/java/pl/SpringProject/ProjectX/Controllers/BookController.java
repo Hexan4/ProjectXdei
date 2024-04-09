c24ea4e1c8005215b9b825889a0f092d594d0c45
Hexan4 <krystian060203@gmail.com>
2024-04-09 19:41:02 +0200

    Conntinue with CRUD using JPA.

--- src/main/java/pl/SpringProject/ProjectX/Controllers/BookController.java
+++ src/main/java/pl/SpringProject/ProjectX/Controllers/BookController.java
@@ -1,13 +1,14 @@
 package pl.SpringProject.ProjectX.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import pl.SpringProject.ProjectX.Data.DataBook;
 import pl.SpringProject.ProjectX.Repository.BookRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/books")
@@ -20,8 +21,47 @@ public class BookController {
         this.bookRepository = bookRepository;
     }
 
+    //Read
     @GetMapping
     public List<DataBook> getAllBooks() {
         return bookRepository.findAll();
+    }
+    //Create
+    @PostMapping
+    public ResponseEntity<DataBook> createBook(@RequestBody DataBook book){
+        DataBook savedBook = bookRepository.save(book);
+        return new ResponseEntity<>(savedBook, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DataBook> getBookById(@PathVariable Long id){
+        Optional<DataBook> book = bookRepository.findById(id);
+        return book.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+    //Update
+    @PutMapping("/{id}")
+    public ResponseEntity<DataBook> updateBook(@PathVariable Long id, @RequestBody DataBook bookDetails){
+        Optional<DataBook> bookData = bookRepository.findById(id);
+
+        if (bookData.isPresent()){
+            DataBook updatedBook = bookData.get();
+            updatedBook.setTitle(bookDetails.getTitle());
+            updatedBook.setAuthor(bookDetails.getAuthor());
+            bookRepository.save(updatedBook);
+            return new ResponseEntity<>(updatedBook,HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    //Delete
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteBook(@PathVariable Long id){
+        try {
+            bookRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/main/java/pl/SpringProject/ProjectX/Repository/BookRepository.java
+++ src/main/java/pl/SpringProject/ProjectX/Repository/BookRepository.java
@@ -3,6 +3,20 @@ package pl.SpringProject.ProjectX.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.SpringProject.ProjectX.Data.DataBook;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<DataBook, Long> {
+    @Override
+    List<DataBook> findAll();
+
+    @Override
+    Optional<DataBook> findById(Long aLong);
+
+    @Override
+    <S extends DataBook> List<S> saveAll(Iterable<S> entities);
+
+    @Override
+    void deleteById(Long aLong);
 
 }
