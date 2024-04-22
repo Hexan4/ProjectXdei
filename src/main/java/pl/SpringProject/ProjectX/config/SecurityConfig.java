e0557cf64baf6d6f6d2b48b0d65e48dcc451bf90
Hexan4 <krystian060203@gmail.com>
2024-04-22 17:00:42 +0200

    spring securit

--- src/main/java/pl/SpringProject/ProjectX/config/SecurityConfig.java
+++ src/main/java/pl/SpringProject/ProjectX/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package pl.SpringProject.ProjectX.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,18 +16,27 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())  // Disable CSRF protection
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/public/**").permitAll()  // Allow public access to all paths under '/public'
-                        .anyRequest().authenticated()  // Require authentication for any other request
-                )
-                .httpBasic(Customizer.withDefaults());  // Use HTTP Basic for authentication
-
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http, PasswordEncoder passwordEncoder) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder
+                .inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder.encode("password"))
+                .authorities("ROLE_USER");
+        return authenticationManagerBuilder.build();
     }
 }
